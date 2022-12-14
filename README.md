c7a5aa337ebb1baae8ec160fae8a11caa97292e3
Ericokim <erickims08@gmail.com>
2022-12-14 12:35:41 +0300

    feat(server.js): server setup

---
+++ README.md
@@ -0,0 +1,29 @@
+# E-nauli Back-Office API
+
+> E-nauli Back-Office platform, experience API.
+
+## Install Dependencies
+
+```
+npm install
+```
+
+## Run App
+
+```
+# Run in dev mode
+npm run dev
+
+# Run in prod mode
+npm start
+```
+
+## Demo
+
+<!-- The API is live at [devcamper-api-pi](https://devcamper-api-pi.vercel.app/)
+
+Extensive documentation with examples [here](https://documenter.getpostman.com/view/2628723/UVsEWVF6)
+
+- Version: 1.0.0
+- License: MIT
+- Author: Eric Kimathi -->
---
+++ server.js
@@ -0,0 +1,90 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+const errorHandler = require("./middleware/error");
+// const swaggerDocs = require("./swagger");
+// const swaggerUI = require("swagger-ui-express");
+
+// Load env vars
+dotenv.config();
+
+// Route files
+const auth = require("./routes/auth");
+const fleet = require("./routes/fleet");
+const sacco = require("./routes/sacco");
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Body-parser --responsible for parsing the incoming request bodies in a middleware before you handle it
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Set security headers
+app.use(helmet({ contentSecurityPolicy: false }));
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiter
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+app.set("trust proxy", 1);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Mount Proxy endpoints routers
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/fleet", fleet);
+app.use("/api/v1/sacco", sacco);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.underline.bold);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
