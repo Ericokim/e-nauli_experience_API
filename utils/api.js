e7bb45181815ec1c7e00b6bd2108ebcf97811e37
Ericokim <erickims08@gmail.com>
2022-12-15 09:57:56 +0300

    perf(API.js): API URL

--- utils/api.js
+++ utils/api.js
@@ -10,23 +10,23 @@ const api = axios.create({
 });
 
 // request interceptor to add the auth token header to requests
-// api.interceptors.request.use(
-//   async (config) => {
-//     // Do something before request is sent
-//     const formData = {
-//       apiKey: API_KEY,
-//       appSecret: APP_SECRET,
-//     };
+api.interceptors.request.use(
+  async (config) => {
+    // Do something before request is sent
+    const formData = {
+      apiKey: API_KEY,
+      appSecret: APP_SECRET,
+    };
 
-//     const { data } = await axios.post(`${API_BASE_URL}/auth/token`, formData);
-//     let token = data.data[0]?.accessToken;
+    const { data } = await axios.post(`${API_BASE_URL}/auth/token`, formData);
+    let token = data.data[0]?.accessToken;
 
-//     config.headers["Authorization"] = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => {
-//     Promise.reject(error);
-//   }
-// );
+    config.headers["Authorization"] = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 module.exports = api;
